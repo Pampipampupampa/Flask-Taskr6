220ce5da8e8c20b5be4ec957f9b15b523add35e1
Pampipampupampa <jeremybois@rocketmail.com>
2015-06-13 12:01:08 +0200

    Add rest API to allow task recuperation using json format

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,16 @@
     Run test first and then add, commit and push to repository.
 """
 
-from fabric.api import local
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
 
-
+# Preparation
 def test():
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm("Tests failed. Continue and commit changes ?"):
+        abort("Aborted at user request.")
+
     local("nosetests -v")
 
 
@@ -25,3 +31,29 @@ def prepare():
     test()
     commit()
     push()
+
+
+# Deploying
+def heroku():
+    local("git push heroku master")
+
+
+def heroku_test():
+    local("heroku run nosetests -v")
+
+
+def pull():
+    local("git pull origin master")
+
+
+def deploy():
+    # pull()
+    test()
+    # commit()
+    heroku()
+    heroku_test()
+
+
+# Rollback if test does not pass on heroku
+def rollback():
+    local("heroku rollback")
--- project/__init__.py
+++ project/__init__.py
@@ -13,10 +13,13 @@ db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
 from project.tasks.views import tasks_blueprint
+from project.api.views import api_blueprint
+
 
 # Register our blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+app.register_blueprint(api_blueprint)
 
 
 # Add error handler
